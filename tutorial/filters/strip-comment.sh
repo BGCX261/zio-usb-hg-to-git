fc8e0b308f9bda092218e91df131a142417399e9
Vijay Kumar <vijaykumar@bravegnu.org>
2013-09-01 08:33:56 +0530

    Fix incorrect comparison operator int comment strip shell script.

--- tutorial/filters/strip-comment.sh
+++ tutorial/filters/strip-comment.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-if [ $1 == "python" ]; then
+if [ "$1" = "python" ]; then
     sed -e "s/# <\([0-9]\+\)>/<\1>/"
-elif [ $1 == "java" ]; then
+elif [ "$1" = "java" ]; then
     sed -e "s/\/\/ <\([0-9]\+\)>/<\1>/" 
 else
     cat
